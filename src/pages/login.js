9752de1849f80e0f83cea234e699b292812b8b55
Mr-Mooo <hjchoujiacheng@126.com>
2020-09-05 23:09:56 +0800

    fix login

--- src/pages/login.js
+++ src/pages/login.js
@@ -74,7 +74,7 @@ class Login extends React.PureComponent {
         duration: 2,
       });
 
-      // sessionStorage.setItem('token', loginRes.access_token);
+      sessionStorage.setItem('token', loginRes.data.access_token);
       sessionStorage.setItem("mobile", loginRes.data.mobile);
       sessionStorage.setItem("email", loginRes.data.email);
       sessionStorage.setItem("nick_name", loginRes.data.nick_name);
